022c815dbd07c72593824b04a84caa96916aee57
ishanahmad90 <ishankhn7860@gmail.com>
2025-02-12 02:14:21 +0530

    update code

--- oops_project_1/inheritance.dart
+++ oops_project_1/inheritance.dart
@@ -1,5 +1,5 @@
 // here the parent is the super class for the child
-class Parent {
+class Parent extends Object {
   int age;
   String name;
 
@@ -13,6 +13,7 @@ class Child extends Parent {
   Child({required super.age, required super.name});
   void display() {
     print(age);
+    print(name);
   }
 }
 
@@ -20,5 +21,6 @@ void main() {
   Child child = Child(age: 12, name: "Ishan");
   child.display();
   child.age = 15;
+  child.name = "Jon";
   child.display();
 }
