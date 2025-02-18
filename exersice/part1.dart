032699f6cec1e0f55837087fe6d6f377493c895a
ishanahmad90 <ishankhn7860@gmail.com>
2025-02-18 13:42:56 +0530

    update code

---
+++ exersice/part1.dart
@@ -0,0 +1,19 @@
+// import 'dart:io';
+
+void main() {
+  // Declare Variables
+  int temperature = 28;
+  double humidity = 65.5;
+  String condition = 'Partly Cloudy';
+  bool isRaining = false;
+  // ----------------
+  String weatherReport = 'Weather Report:' +
+      ' Temperature: $temperature°C' +
+      'Humidity: $humidity%\n Condition: $condition\n Raining: $isRaining';
+
+  print(weatherReport);
+  var a = 10;
+  var b = 3;
+  var c = a / b;
+  print(c);
+}
